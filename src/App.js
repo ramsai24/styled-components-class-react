53c76576121a48e61af7114131d82a3218f2311d
jnrsai24 <jnrsai24@gmail.com>
2023-08-20 12:50:53 +0000

    class completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import {Heading, CustomButton} from './styledComponents'
 import './App.css'
 
-const App = () => <h1 className="heading">Hello World</h1>
+const App = () => (
+  <>
+    <Heading className="heading">Hello World</Heading>
 
+    {/* <CustomButton type="button" color="#ffffff" bgColor="#0070c1">
+      Click
+    </CustomButton> */}
+
+    {/* <CustomButton type="button" outline={false}>
+      Click
+    </CustomButton> */}
+
+    <CustomButton type="button">Click</CustomButton>
+
+    {/* <CustomButton type="button" bgColor="#ffffff" color="#0070c1">
+      Click
+    </CustomButton> */}
+
+    {/* <CustomButton type="button" outline={true}>
+      Click
+    </CustomButton> */}
+
+    <CustomButton type="button" outline>
+      Click
+    </CustomButton>
+  </>
+)
 export default App
---
+++ src/styledComponents.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const Heading = styled.h1`
+  color: #0070c1;
+  font-family: 'Roboto';
+`
+
+export const CustomButton = styled.button`
+  padding: 10px;
+  margin-right: 20px;
+  font-size: 15px;
+  //   color: #ffffff;
+  // color:${props => props.color};
+  color: ${props => (props.outline ? '#0070c1' : '#ffffff')};
+  border-radius: 4px;
+  border: 2px solid #0070c1;
+  //   background-color: #0070c1;
+  // background-color:${props => props.bgColor};
+  background-color: ${props => (props.outline ? '#ffffff' : '#0070c1')};
+`
